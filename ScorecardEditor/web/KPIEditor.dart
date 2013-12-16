5c428257fc8ea9202f8cf4f3bfa954686902e56f
PhiLho <PhiLho@GMX.net>
2013-12-16 07:37:55 +0100

    ScorecardEditor: rename RangeControl to KPIEditor.
    Also add a field to type the KPI name.

--- ScorecardEditor/web/KPIEditor.dart
+++ ScorecardEditor/web/KPIEditor.dart
@@ -2,14 +2,14 @@ import 'package:polymer/polymer.dart';
 //import 'RangeSelector.dart';
 
 /**
- * Scorecard range control.
+ * Scorecard's editor for a KPI.
  */
-@CustomTag('range-control')
-class RangeControl extends PolymerElement
+@CustomTag('kpi-editor')
+class KPIEditor extends PolymerElement
 {
 	final List selectors = toObservable([1, 2]);
 	@observable int rangeNb = 2;
 
-	RangeControl.created() : super.created() {}
+	KPIEditor.created() : super.created() {}
 }
 
